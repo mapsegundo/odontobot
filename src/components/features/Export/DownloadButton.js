c385616a34727db47015f05611d8367a793b1810
Marshall Paiva <marshallpaiva@hotmail.com>
2025-03-04 19:58:16 -0300

    Restauração: Estilo azul dos títulos e botões, aprimoramento das bordas e correção do tema

--- src/components/features/Export/DownloadButton.js
+++ src/components/features/Export/DownloadButton.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./styles.css";
 
 const DownloadButton = ({ downloadPDF }) => {
   return (
     <div className="text-center">
       <button
-        className="btn vw-50 btn-lg text-center m-3 saidaButton"
+        className="btn btn-primary btn-lg text-center m-3 download-button"
         onClick={downloadPDF}
       >
         Baixar em PDF
